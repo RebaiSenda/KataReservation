bc8aab8c96ff1c6fcc33f8076259381c9cf541a9
RebaiSenda <rebaisenda93@gmail.com>
2025-03-28 14:56:21 +0100

    updare person service

--- KataReservation.Web/katareservation.web.client/src/app/services/person.service.ts
+++ KataReservation.Web/katareservation.web.client/src/app/services/person.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Person } from '../models/person';
 import { environment } from '../../environments/environment';
 
@@ -12,9 +12,29 @@ export class PersonService {
   constructor(private http: HttpClient) { }
 
   getPersons(): Observable<Person[]> {
-    return this.http.get<Person[]>(environment.apiUrl);
+    var response = this.http.get<Person[]>(environment.apiUrl);
+
+        return this.ToPersonModel(response);
+  }
+  // Implementation of ToPersonModel function
+ ToPersonModel(response: Observable<any[]>): Observable<Person[]> {
+    return response.pipe(
+      map(data => data.map(item => ({
+        id: item.id || 0,
+        firstName: this.(item.name || item.firstName || ''),
+        lastName: this.extractLastName(item.name || item.lastName || '')
+      })))
+    );
+  }
+  private extractFirstName(fullName: string): string {
+    return fullName.split(' ')[0] || '';
   }
 
+  // Helper method to extract last name
+  private extractLastName(fullName: string): string {
+    const nameParts = fullName.split(' ');
+    return nameParts.length > 1 ? nameParts.slice(1).join(' ') : '';
+  }
   getPerson(id: number): Observable<Person> {
     return this.http.get<Person>(`${environment.apiUrl}/${id}`);
   }
